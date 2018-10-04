cba6a2b3df4469d57cfee3972c3ce50c38863318
Kelly <devkells00@gmail.com>
2018-10-04 16:07:10 -0500

    Done

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,18 @@
 // Code ControlledInput Component Here
+handleFirstNameChange = event => {
+  this.setState({
+    firstName: event.target.value
+  })
+ }
+  
+ handleLastNameChange = event => {
+  this.setState({
+    lastName: event.target.value
+  })
+ }
+
+ handleChange = (e) => {
+   this.setState({
+     [event.target.name]: event.target.value
+   })
+ }
